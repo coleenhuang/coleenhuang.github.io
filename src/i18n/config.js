27e95f0856263275c895090baf1192fcb8096344
Coleen Huang <coleenhuang@gmail.com>
2020-04-28 14:34:51 -0700

    added language detector

--- src/i18n/config.js
+++ src/i18n/config.js
@@ -1,6 +1,7 @@
 import i18next from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector'
 
-i18next.init({
+i18next.use(LanguageDetector).init({
   fallbackLng: 'en',
   resources: {
     zh: {
